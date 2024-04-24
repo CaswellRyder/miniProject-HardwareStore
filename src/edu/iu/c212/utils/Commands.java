db42a60045738ed10fcbb84c7a22da5558537792
Ryder Caswell <68606911+CaswellRyder@users.noreply.github.com>
2024-04-24 12:35:49 -0400

    changes made yay

---
+++ src/edu/iu/c212/utils/Commands.java
@@ -0,0 +1,44 @@
+package edu.iu.c212.utils;
+
+public class Commands {
+    public void firePerson() {
+        System.out.println("This is the person that is being fired");
+    }
+    public void hirePerson() {
+        System.out.println("This is the person that is being hired");
+    }
+    public void promotePerson() {
+        System.out.println("This is the person that is being promoted");
+    }
+    public void sellItem() {
+        System.out.println("This is the item that is being sold");
+    }
+
+    public void quantityOfItem() {
+        System.out.println("This is the quantity of the item");
+    }
+
+    public void addItem() {
+        System.out.println("This adds the item");
+    }
+
+    public void costOfItem() {
+        System.out.println("This is the cost of the item");
+    }
+
+    public void exitApp() {
+        System.out.println("This is the exit of the app");
+    }
+
+    public void findItem() {
+        System.out.println("This is finds the item");
+    }
+
+    public void sawPlank() {
+        System.out.println("This is the sawing of the plank");
+    }
+
+    public void scheduleStaff() {
+        System.out.println("This is the schedule of the staff");
+    }
+}
