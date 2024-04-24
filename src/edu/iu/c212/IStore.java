b2f8dcce5acb38f7df5ae885c3ab890d2bc8dc37
BenJacobs24 <145147983+BenJacobs24@users.noreply.github.com>
2024-04-24 11:43:00 -0400

    Update IStore.java

--- src/edu/iu/c212/IStore.java
+++ src/edu/iu/c212/IStore.java
@@ -1,5 +1,13 @@
 package edu.iu.c212;
 
 public interface IStore {
-
+	List<Item> getItemsFromFile();
+	
+	List<Staff> getStaffFromFile();
+	
+	void saveItemsFromFile();
+	
+	void saveStaffFromFile();
+	
+	void takeAction();
 }
